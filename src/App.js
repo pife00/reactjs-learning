6f66f125d7724660a2756c8ee49e9130c0c3f07e
pife00 <pife00@gmail.com>
2021-01-25 15:11:36 -0500

    chapter 2

--- src/App.js
+++ src/App.js
@@ -4,37 +4,106 @@ import Person from "./Person/Person";
 
 class App extends React.Component {
   state = {
-    person:[
-      {name:"Pipe",age:27}
-    ]
-  }
-  changeName = (newName) =>{
+    person: [
+      { id:'1', name: "Pipe", age: 27 },
+      { id:'2', name: "Nadia", age: 21 },
+      { id:'3', name: "Emperatriz", age: 16 },
+    ],
+    showPerson: false,
+  };
+  deletePerson = (personIndex) => {
+    const persons = this.state.person.slice();
+    persons.splice(personIndex,1);
+    this.setState({person:persons});
+  };
+
+  nameHandler = (event,id) => {
+    const personIndex = this.state.person.findIndex(el=>{
+      return el.id === id
+    });
+
+    const person = {
+      ...this.state.person[personIndex]
+    }
+
+    person.name = event.target.value;
+
+    const persons = this.state.person
+    persons[personIndex] = person
+
+    this.setState({person: persons})
+
+
+  };
+
+  keyHandler = (value) => {
+    console.log(value);
+  };
+
+  togglePerson = () => {
+    const doesShow = this.state.showPerson;
     this.setState({
-    person:[
-      {name:newName,age:27}
-    ]
-    })
-  }
+      showPerson: !doesShow,
+    });
+  };
 
-  keyHandler = (value) =>{
-    console.log(value)
-  }
+  addPerson = () => {};
 
-  render(){
-    return( 
-      <div className="App" >
-        <div className="constrain" >
-        <h1>Hello</h1>
-        <hr/>
-        <button onClick={this.changeName} >Switch name</button>
-        <Person
-        click={this.changeName.bind(this,'Marle')} 
-        name={this.state.person[0].name} 
-        age={this.state.person[0].age}
-        > Hello bb </Person>
+  render() {
+    const style = {
+      backgroundColor: "white",
+      border: "1x solid blue",
+    };
+
+    let persons = null;
+    if (this.state.showPerson) {
+      persons = (
+        <div>
+          {this.state.person.map((person,index) => {
+            return (
+              <Person
+                name={person.name}
+                age={person.age}
+                key={person.id}
+                change={(event) => this.nameHandler(event,person.id)}
+                click={ ()=> this.deletePerson(index)}
+              />
+            );
+
+          })}
+        </div>
+      );
+      style.backgroundColor = 'green'
+      style.color = 'white'
+    }
+
+    let classes = [];
+    if(this.state.showPerson){
+      classes.push('blue')
+    }else{
+      classes.push('bold')
+    }
+
+    return (
+      <div className="App">
+        <div className="constrain">
+          <h1 className={classes} >Hello</h1>
+          <hr />
+          <button style={style} onClick={this.togglePerson}>
+            Switch name
+          </button>
+
+          <button style={style} onClick={this.addPerson}>
+            Add Person
+          </button>
+          {this.state.showPerson ? (
+            <div>
+              {persons}
+            </div>
+          ) : null}
         </div>
       </div>
-    )
+    );
   }
 }
 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -10,7 +10,7 @@ const person = (props) => {
     <div>
       <p onClick={props.click} >Hello Today I am {props.name} I am {props.age}  </p>
       <h1> {props.children} </h1>
-      <input onKeyPress={pressKey} type="text" />
+      <input onChange={props.change} value={props.name} type="text" />
     </div>
   );
 };
